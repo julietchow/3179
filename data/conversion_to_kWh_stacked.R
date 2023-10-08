bdc0eee2bdf9ef5bbf0ba87086e51064f9a9da84
julietchow <jcho0109@student.monash.edu>
2023-10-08 15:34:32 +1100

    change

---
+++ data/conversion_to_kWh_stacked.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+data <- read.csv("stacked_bar_chart.csv")
+
+# Define the conversion factors
+# Define the conversion factor
+quad_to_kWh <- 293.0711
+
+# Convert multiple columns to kWh
+data <- data %>%
+  mutate(
+    count = count * quad_to_kWh
+  )
+
+# Define the file name
+output_file <- "stacked_bar_chart_new.csv"
+
+# Save the dataframe as a CSV file
+write.csv(data, file = output_file, row.names = FALSE)
+
--- data/stacked_area_chart.R
+++ data/stacked_area_chart.R
@@ -1,3 +1,8 @@
+# Load the tidyverse package if it's not already loaded
+# install.packages("tidyverse") and ("dplyr")  # Uncomment and run if not installed
+library(tidyverse)
+library(dplyr)
+
 #Coal
 coal <- read.csv("Consumption_Coal.csv", strip.white = TRUE)
 
@@ -10,11 +15,31 @@ colnames(coal) <- new_column_names
 # Assuming your data frame is named coal, you can subset it to get columns from X2000 to X2021:
 coal <- coal[, c("Continent", "Country", paste0(2000:2021))]
 
+# Assuming your data frame is named coal, you can clean up the "Country" column
+coal <- coal %>%
+  mutate(Country = str_trim(Country, side = "both"))
+
+# Reshape the data as before
+reshaped_coal <- coal %>%
+  pivot_longer(cols = -c(Continent, Country), names_to = "Year", values_to = "count") %>%
+  mutate(Year = as.numeric(Year))
+
+# Delete the "Country" column using subset()
+reshaped_coal <- subset(reshaped_coal, select = -Country)
+
+# Assuming your data frame is named 'reshaped_coal' and the column is named 'COAL_consumption'
+reshaped_coal$count <- ifelse(reshaped_coal$count == "--", 0, reshaped_coal$count)
+
+reshaped_coal <- reshaped_coal %>%
+  group_by(Continent, Year) %>%
+  mutate(count = as.numeric(count)) %>%
+  summarise(count = sum(count, na.rm = TRUE))
+
 # Add a new column "Energy Source" with all values set to "coal"
-coal['Energy Source'] = 'Coal'
+reshaped_coal['Energy Source'] = 'Coal'
 
 # Reorder the columns
-coal <- coal %>%
+reshaped_coal <- reshaped_coal %>%
   select("Energy Source", everything())
 
 #Natural Gas
@@ -29,11 +54,28 @@ colnames(ng) <- new_column_names
 # Assuming your data frame is named coal, you can subset it to get columns from X2000 to X2021:
 ng <- ng[, c("Continent", "Country", paste0(2000:2021))]
 
+# Reshape the data as before
+reshaped_ng <- ng %>%
+  pivot_longer(cols = -c(Continent, Country), names_to = "Year", values_to = "count") %>%
+  mutate(Year = as.numeric(Year))
+
+# Delete the "Country" column using subset()
+reshaped_ng <- subset(reshaped_ng, select = -Country)
+
+# Assuming your data frame is named 'reshaped_ng' and the column is named 'COAL_consumption'
+reshaped_ng$count <- ifelse(reshaped_ng$count == "--", 0, reshaped_ng$count)
+reshaped_ng$count <- ifelse(reshaped_ng$count == "ie", 0, reshaped_ng$count)
+
+reshaped_ng <- reshaped_ng %>%
+  group_by(Continent, Year) %>%
+  mutate(count = as.numeric(count)) %>%
+  summarise(count = sum(count, na.rm = TRUE))
+
 # Add a new column "Energy Source" with all values set to "coal"
-ng['Energy Source'] = 'Natural Gas'
+reshaped_ng['Energy Source'] = 'Natural Gas'
 
 # Reorder the columns
-ng <- ng %>%
+reshaped_ng <- reshaped_ng %>%
   select("Energy Source", everything())
 
 #Neuclear+renewables
@@ -48,11 +90,27 @@ colnames(nr) <- new_column_names
 # Assuming your data frame is named coal, you can subset it to get columns from X2000 to X2021:
 nr <- nr[, c("Continent", "Country", paste0(2000:2021))]
 
+# Reshape the data as before
+reshaped_nr <- nr %>%
+  pivot_longer(cols = -c(Continent, Country), names_to = "Year", values_to = "count") %>%
+  mutate(Year = as.numeric(Year))
+
+# Delete the "Country" column using subset()
+reshaped_nr <- subset(reshaped_nr, select = -Country)
+
+# Assuming your data frame is named 'reshaped_nr' and the column is named 'count'
+reshaped_nr$count <- ifelse(reshaped_nr$count == "--", 0, reshaped_nr$count)
+
+reshaped_nr <- reshaped_nr %>%
+  group_by(Continent, Year) %>%
+  mutate(count = as.numeric(count)) %>%
+  summarise(count = sum(count, na.rm = TRUE))
+
 # Add a new column "Energy Source" with all values set to "coal"
-nr['Energy Source'] = 'Nuclear & Renewables'
+reshaped_nr['Energy Source'] = 'Nuclear & Renewables'
 
 # Reorder the columns
-nr <- nr %>%
+reshaped_nr <- reshaped_nr %>%
   select("Energy Source", everything())
 
 #Petroleum
@@ -67,15 +125,31 @@ colnames(p) <- new_column_names
 # Assuming your data frame is named coal, you can subset it to get columns from X2000 to X2021:
 p <- p[, c("Continent", "Country", paste0(2000:2021))]
 
+# Reshape the data as before
+reshaped_p <- p %>%
+  pivot_longer(cols = -c(Continent, Country), names_to = "Year", values_to = "count") %>%
+  mutate(Year = as.numeric(Year))
+
+# Delete the "Country" column using subset()
+reshaped_p <- subset(reshaped_p, select = -Country)
+
+# Assuming your data frame is named 'reshaped_p' and the column is named 'count'
+reshaped_p$count <- ifelse(reshaped_p$count == "--", 0, reshaped_p$count)
+reshaped_p$count <- ifelse(reshaped_p$count == "ie", 0, reshaped_p$count)
+
+reshaped_p <- reshaped_p %>%
+  group_by(Continent, Year) %>%
+  mutate(count = as.numeric(count)) %>%
+  summarise(count = sum(count, na.rm = TRUE))
+
 # Add a new column "Energy Source" with all values set to "coal"
-p['Energy Source'] = 'Petroleum'
+reshaped_p['Energy Source'] = 'Petroleum'
 
 # Reorder the columns
-p <- p %>%
+reshaped_p <- reshaped_p %>%
   select("Energy Source", everything())
 
 # Combine the two tables row-wise
-final_table <- rbind(coal, ng, nr, p)
-final_table["Energy Source"]
+final_table <- rbind(reshaped_coal, reshaped_ng, reshaped_nr, reshaped_p)
 
 write.csv(final_table, file = "stacked_area_chart.csv", row.names = FALSE)

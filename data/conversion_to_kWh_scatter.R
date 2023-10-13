c329891965e8166eb37fe74c77b2e75419a58b5a
julietchow <jcho0109@student.monash.edu>
2023-10-13 11:56:40 +1100

    change

--- data/conversion_to_kWh_scatter.R
+++ data/conversion_to_kWh_scatter.R
@@ -8,11 +8,12 @@ quad_to_kWh <- 293.0711
 # Convert multiple columns to kWh
 data <- data %>%
   mutate(
-    count =  * quad_to_kWh
+    Total_Consumption = Total_Consumption * quad_to_kWh,
+    Total_production = Total_production * quad_to_kWh
   )
 
 # Define the file name
-output_file <- "final3.csv"
+output_file <- "scatter.csv"
 
 # Save the dataframe as a CSV file
 write.csv(data, file = output_file, row.names = FALSE)
--- data/pie_chart.R
+++ data/pie_chart.R
@@ -1,1 +1,31 @@
+# Load the dplyr package if it's not already loaded
+library(dplyr)
+library(tidyr)
+
 data <- read.csv("choropleth_map_new.csv")
+
+# Assuming your data is in a data frame named 'df'
+# Group the data by Year and calculate the sum of each column
+result <- data %>%
+  group_by(Year) %>%
+  summarize(
+    coal_consumption = sum(coal_consumption),
+    ng_consumption = sum(ng_consumption),
+    nr_consumption = sum(nr_consumption),
+    p_consumption = sum(p_consumption)
+  ) %>%
+  ungroup()  # Remove grouping
+
+# Reshape the data
+result <- result %>%
+  gather(key = "Energy_Source", value = "Consumption", -Year)
+
+# Now, df_long contains the data in the desired format
+
+result
+# Define the file name
+output_file <- "pie_chart.csv"
+
+# Save the dataframe as a CSV file
+write.csv(result, file = output_file, row.names = FALSE)
+

b28be1cdfdc54106d56bc92337112466d316fdfb
julietchow <jcho0109@student.monash.edu>
2023-10-14 14:29:45 +1100

    change

---
+++ data/line_chart.R
@@ -0,0 +1,10 @@
+# Assuming your data is stored in a data frame named 'df'
+library(dplyr)
+
+data <- read.csv("stacked_area_chart.csv")
+
+result <- data %>%
+  group_by(Continent, Year) %>%
+  summarise(Total_Energy_Consumption = sum(count))
+
+write.csv(result, "line_chart.csv", row.names = FALSE)
